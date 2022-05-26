40185d61c1e808fa3aa31cf2e8fecd4976b03aa6
James Correa <james.correa9090@gmail.com>
2022-05-26 13:28:35 -1000

    initial commit of some practicing types and variables
    Signed-off-by: James Correa <james.correa9090@gmail.com>

---
+++ hello.js
@@ -0,0 +1,11 @@
+#dumb program to discuss programming types
+let x = 7;
+console.log(x === 5);
+console.log(x === "5");
+
+if (x < 6 ) {
+	 console.log("x is less than six")
+} else {
+	console.log("x is greater than six")
+};
+
